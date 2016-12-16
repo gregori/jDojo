fdec2546f70c9536b9ee97f335bce3807058f440
Rodrigo Gregori <spam@gregori.eti.br>
2016-12-16 08:59:39 -0200

    Inicial commit for the project

---
+++ src/main/java/br/eti/gregori/DojoApplication.java
@@ -0,0 +1,12 @@
+package br.eti.gregori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojoApplication.class, args);
+	}
+}
